8101a56d8d0ee774c4062b9c2b46c26a81de73ae
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-28 22:22:15 +0100

    Add the cartPage Route and calling its widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:men_you/pages/cart_page.dart';
 import 'package:men_you/pages/home_page.dart';
 
 void main(){
@@ -15,7 +17,8 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFF5F5F3),
       ),
       routes: {
-        '/' : (context) => HomePage()
+        '/' : (context) => HomePage(),
+        'cartPage' : (context) => CartPage(),
       },
     );
   }

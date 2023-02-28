283adf915cd76eed63a89ffc040c4918032a0d14
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-28 10:13:47 +0100

    Working on the HomePage screen until now i integrated the AppBar , SearchField and Categories widget , currently working on Popular Items widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:men_you/widgets/categories_widget.dart';
+import 'package:men_you/widgets/search_field_widget.dart';
 
 import '../widgets/app_bar_widget.dart';
+import '../widgets/popular_items_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -9,6 +12,33 @@ class HomePage extends StatelessWidget {
         body: ListView(
           children: [
             AppBarWidget(),
+            // Search Field Widget
+            SearchFieldWidget(),
+            // Category
+            const Padding(
+              padding: EdgeInsets.only(top: 20 , left: 10),
+              child: Text(
+                  'Categories',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            CategoriesWidget(),
+            // Popular Items
+            const Padding(
+              padding: EdgeInsets.only(top: 20 , left: 10),
+              child: Text(
+                'Popular',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            // Popular Items Widget
+            PopularItemsWidget()
           ],
         ),
     );

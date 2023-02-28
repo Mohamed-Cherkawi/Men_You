4774ba05b7bc762d9eb174b7a40e32e73ca15580
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-28 13:36:20 +0100

    Create a class that holds a popular item widget's data to be used

---
+++ lib/util/popular_item.util.dart
@@ -0,0 +1,9 @@
+class PopularItemData {
+  final String imageFileName;
+  final String itemTitle;
+  final String itemDescription;
+  final int itemPrice;
+
+   PopularItemData(
+      this.imageFileName, this.itemTitle, this.itemDescription, this.itemPrice);
+}
